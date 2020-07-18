b3e6f69d60186d08333e06a6feea7658bf835fed
psnipiv <psnipiv@.github.com>
2020-07-18 20:17:33 +0800

    Changes with 2020 data
    Changes with 2020 data

--- app.py
+++ app.py
@@ -4,58 +4,102 @@ import numpy as np
 import json
 import sys
 import matplotlib.pyplot as plt
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
 import plotly.express as px
 import statsmodels.api as sm
 
 
 def main():
-    df_d1S1 = load_data_session('d1S1')
-    df_d1S2 = load_data_session('d1S2')
-    df_d2S1 = load_data_session('d2S1')
-    df_d2S2 = load_data_session('d2S2')
-    df_d3S1 = load_data_session('d3S1')
-    df_d3S2 = load_data_session('d3S2')
+
+    df_r1M = load_data_session('r1M')
+    df_r1P1 = load_data_session('r1P1')
+    df_r1P2 = load_data_session('r1P2')
+    df_r1P3 = load_data_session('r1P3')
+
+    df_r2M = load_data_session('r2M')
+    df_r2P1 = load_data_session('r2P1')
+    df_r2P2 = load_data_session('r2P2')
+    df_r2P3 = load_data_session('r2P3')
 
 
-    page = st.sidebar.selectbox("Choose a page", ['Home Page','F1 Testing 2' ])
+    page = st.sidebar.selectbox("Choose a page", ['Home Page','1-Austria GP','2-Styria GP' ])
 
     if page == 'Home Page':
-        st.title('Barcelona Test 2')
-        st.text('Select a page in the sidebar')
-        st.write("Here is the interactive timing charts for lap times clocked by drivers of respective team during the Formula 1 Test 2 2020 at Barcelona. However, there are many unknowns in the testing and details of tyres and sprints were not completely provided by FIA. Let's wait for couple of weeks to find the fastest car.")
+        st.title('Formula 1 2020 Season')
+        st.write("")
+        st.write("Here is the interactive timing charts for lap times clocked by drivers of respective team during the Formula 1 2020 season.")
         st.write('')
+        st.write("")
+        st.text('Select a page in the sidebar')
         st.write("")
         st.write("Note : These charts are best to view in bigger screens")
         st.write("The interactive charts are created using plotly and the devlopment of user interface is done with streamlit nd deployed in heroku")
 
-
-
-
-    elif page == 'F1 Testing 2':
+    elif page == '1-Austria GP':
         # SelectBox
-        testdayno = st.selectbox("Select Day",["Day 1","Day 2","Day 3"])
+        testdayno = st.selectbox("Select Session",["Practice","Main Race"])
         st.write("On ", testdayno)
 
-        if testdayno == "Day 1":
-            sessionno = st.radio("Select session",("Morning","Afternoon"))
-            if sessionno =="Morning":
-                load_plot1(df_d1S1,0,90)
-            elif sessionno =="Afternoon":
-                load_plot1(df_d1S2,0,120)
-        elif testdayno == "Day 2":
-            sessionno = st.radio("Select session",("Morning","Afternoon"))
-            if sessionno =="Morning":
-                load_plot1(df_d2S1,0,50)
-            elif sessionno =="Afternoon":
-                load_plot1(df_d2S2,0,160)
-        elif testdayno == "Day 3":
-            sessionno = st.radio("Select session",("Morning","Afternoon"))
-            if sessionno =="Morning":
-                load_plot1(df_d3S1,0,90)
-            elif sessionno =="Afternoon":
-                load_plot1(df_d3S2,0,190)
+        if testdayno == "Practice":
+            sessionno = st.radio("Select Practice session",("Practice 1","Practice 2","Practice 3"))
+            if sessionno =="Practice 1":
+                if df_r1P1.empty:
+                    st.write("Session Data is not available.")
+                else:
+                    load_plot1(df_r1P1,0,50)
+            elif sessionno =="Practice 2":
+                if df_r1P2.empty:
+                    st.write("Session Data is not available.")
+                else:
+                    load_plot1(df_r1P2,0,55)
+            elif sessionno =="Practice 3":
+                if df_r1P3.empty:
+                    st.write("Session Data is not available.")
+                else:
+                    load_plot1(df_r1P3,0,25)
+        elif testdayno == "Main Race":
+            if df_r1M.empty:
+                st.write("Session Data is not available.")
+            else:
+                st.write(df_r1M)
+                load_plot2(df_r1M,1,75,60,130)
+                load_plot3(df_r1M,1,75,60,80)
+                load_plot4(df_r1M,1,75)
+        else:
+            st.write("Session Data is not available.")
+
+
+    elif page == '2-Styria GP':
+        # SelectBox
+        testdayno = st.selectbox("Select Practice Session",["Practice","Main Race"])
+        st.write("On ", testdayno)
+
+        if testdayno == "Practice":
+            sessionno = st.radio("Select session",("Practice 1","Practice 2","Practice 3"))
+            if sessionno =="Practice 1":
+                if df_r2P1.empty:
+                    st.write("Session Data is not available.")
+                else:
+                    load_plot1(df_r2P1,0,45)
+            elif sessionno =="Practice 2":
+                if df_r2P2.empty:
+                    st.write("Session Data is not available.")
+                else:
+                    load_plot1(df_r2P2,0,50)
+            elif sessionno =="Practice 3":
+                if df_r2P3.empty:
+                    st.write("Session Data is not available.")
+                else:
+                    load_plot1(df_r2P3,0,50)
+        elif testdayno == "Main Race":
+            if df_r2M.empty:
+                st.write("Session Data is not available.")
+            else:
+                st.write(df_r2M)
+                load_plot2(df_r2M,1,75,60,130)
+                load_plot3(df_r2M,1,75,60,80)
+                load_plot4(df_r2M,1,75)
+        else:
+            st.write("Session Data is not available.")
         
     else:
         st.text('Select a page in the sidebar')
@@ -65,18 +109,23 @@ def main():
 def load_data_session(session):
     sessionfile = ''
     jsondata = '{}'
-    if session == 'd1S1':
-        sessionfile = 'Export_DataFrame_D1S1.json'
-    elif session == 'd1S2':
-        sessionfile = 'Export_DataFrame_D1S2.json'
-    elif session == 'd2S1':
-        sessionfile = 'Export_DataFrame_D2S1.json'
-    elif session == 'd2S2':
-        sessionfile = 'Export_DataFrame_D2S2.json'
-    elif session == 'd3S1':
-        sessionfile = 'Export_DataFrame_D3S1.json'
-    elif session == 'd3S2':
-        sessionfile = 'Export_DataFrame_D3S2.json'
+    if session == 'r1P1':
+        sessionfile = '1-Final_Practice1Data.json'
+    elif session == 'r1P2':
+        sessionfile = '1-Final_Practice2Data.json'
+    elif session == 'r1P3':
+        sessionfile = '1-Final_Practice3Data.json'
+    elif session == 'r1M':
+        sessionfile = '1-Final_MainRaceData.json'
+
+    elif session == 'r2P1':
+        sessionfile = '2-Final_Practice1Data.json'
+    elif session == 'r2P2':
+        sessionfile = '2-Final_Practice2Data.json'
+    elif session == 'r2P3':
+        sessionfile = '2-Final_Practice3Data.json'
+    elif session == 'r2M':
+        sessionfile = '2-Final_MainRaceData.json'
         
     with open(sessionfile) as json_file:
         jsondata = json.load(json_file)
@@ -84,9 +133,27 @@ def load_data_session(session):
     return data
 
 def load_plot1(df,startlap,endlap):
-    fig = px.scatter(df, x="LAPS", y="S123", color="N",template="ggplot2",width=1200,height=600, hover_name="CURRENTLAP",trendline="lowess")
+    fig = px.scatter(df, x="LAPS", y="S123", color="NAME",template="ggplot2",width=1200,height=600, hover_name="TYRE",trendline="lowess")
     fig.update_xaxes(range=[startlap,endlap],title_text='Lap Number')
-    fig.update_yaxes(range=[75,175],title_text='Total sector time')
+    fig.update_yaxes(range=[60,150],title_text='Total sector time')
+    st.plotly_chart(fig)
+
+def load_plot2(df,startlap,endlap,mintime,maxtime):
+    fig = px.scatter(df, x="LAPNO", y="S123", color="DRIVERCODE",template="ggplot2",width=1200,height=600, hover_name="TYRE",trendline="lowess")
+    fig.update_xaxes(range=[startlap,endlap],title_text='Lap Number')
+    fig.update_yaxes(range=[mintime,maxtime],title_text='Total sector time')
+    st.plotly_chart(fig)
+
+def load_plot3(df,startlap,endlap,mintime,maxtime):
+    fig = px.scatter(df, x="LAPNO", y="S123", animation_frame="LAPNO", animation_group="NAME",size="S123", color="NAME", hover_name="CURRENTLAP", size_max=10,width=1200,height=600,range_y=[mintime,maxtime],range_x=[startlap,endlap])
+    fig.update_xaxes(title_text='Lap Number')
+    fig.update_yaxes(title_text='Total sector time')
+    st.plotly_chart(fig)
+
+def load_plot4(df,startlap,endlap):
+    fig = px.scatter(df, x="LAPNO", y="P", animation_frame="LAPNO", animation_group="NAME",size="P", color="NAME", hover_name="CURRENTLAP", size_max=20,width=1200,height=600,range_y=[1,20],range_x=[startlap,endlap])
+    fig.update_xaxes(title_text='Lap Number')
+    fig.update_yaxes(title_text='Leader Board')
     st.plotly_chart(fig)
 
 if __name__ == '__main__':
